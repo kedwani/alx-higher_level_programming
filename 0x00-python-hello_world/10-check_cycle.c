8c2807ab19a42b43cf483228e5b3306268d70911
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-27 22:36:04 +0200

    auto push

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+/**
+ *check_cycle- is a fun
+ *
+ *@list : is he header.
+ *
+ *Return:- 0 or 1.
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *p[1024];
@@ -8,21 +16,20 @@ int check_cycle(listint_t *list)
 /*intialize the arrray */
 	p[0] = h;
 	p[1] = NULL;
-	for (i=0;h->next != NULL;++i)
+	for (i = 0; h->next != NULL; ++i)
 	{
-		for (j=0;p[j] != NULL;++j)
+		for (j = 0; p[j] != NULL; ++j)
 		{
 			if (h->next == p[i])
 				return (1);
-			if (p[i+1] == NULL)
+			if (p[i + 1] == NULL)
 			{
-				p[i+1] = h->next;
-				p[i+2] = NULL;
+				p[i + 1] = h->next;
+				p[i + 2] = NULL;
 				break;
 			}
 		}
 		h = h->next;
 	}
-	return(0);
-
+	return (0);
 }
