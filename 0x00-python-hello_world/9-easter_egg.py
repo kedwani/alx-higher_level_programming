a316c4f85589e2b5b42a9645a9d6227b36dad41c
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-27 21:54:27 +0200

    auto push

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,12 +1,2 @@
 #!/usr/bin/python3
-i = " is better than "
-print("The Zen of Python, by Tim Peters\nBeautiful{i}ugly./nExplicit{i}implicit\
-.\nSimple{i}complex./nComplex{i}complicated.\nFlat{i}nested.\nSparse{i}dense.\n\
-Readability counts.\nSpecial cases aren't special enough to break the rules.\n\
-Although practicality beats purity.\nErrors should never pass silently.\nUnless\
- explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess\
-.\nThere should be one-- and preferably only one --obvious way to do it.\nAltho\
-ugh that way may not be obvious at first unless you're Dutch.\nNow{i}never.\nAl\
-though never is often {i[5:]}*right* now.\nIf the implementation is hard to exp\
-lain, it's a bad idea.\nIf the implementation is easy to explain, it may be a \
-good idea.\nNamespaces are one honking great idea -- let's do more of those!")
+import this
