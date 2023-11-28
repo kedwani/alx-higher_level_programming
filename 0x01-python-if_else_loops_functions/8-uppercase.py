f7d1bc94f459be447ef03b89daaf68e3df72ce8c
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-28 20:08:06 +0200

    auto push

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for o, i in enumerate(str):
         if (ord(i) >= 97 and ord(i) < 123):
-            i = chr(ord(i) + 55)
+            i = chr(ord(i) - 55)
         if (o == len(str) - 1):
             print("{}".format(i))
         else:
