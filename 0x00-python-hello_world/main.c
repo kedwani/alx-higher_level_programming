0d7511d56023ba352a25f4f32cd1c6276b72fb77
kedwani <windows@linux.com>
2023-11-28 09:44:42 +0200

    auto push

--- 0x00-python-hello_world/main.c
+++ 0x00-python-hello_world/main.c
@@ -3,55 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
+#include <time.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    listint_t *head;
-    listint_t *current;
-    listint_t *temp;
-    int i;
 
-    head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint(head);
-
-    if (check_cycle(head) == 0)
-        printf("Linked list has no cycle\n");
-    else if (check_cycle(head) == 1)
-        printf("Linked list has a cycle\n");
-
-    current = head;
-    for (i = 0; i < 4; i++)
-        current = current->next;
-    temp = current->next;
-    current->next = head;
-
-    if (check_cycle(head) == 0)
-        printf("Linked list has no cycle\n");
-    else if (check_cycle(head) == 1)
-        printf("Linked list has a cycle\n");
-
-    current = head;
-    for (i = 0; i < 4; i++)
-        current = current->next;
-    current->next = temp;
-
-    free_listint(head);
-
-    return (0);
-}
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
@@ -105,4 +63,45 @@ void free_listint(listint_t *head)
         head = head->next;
         free(current);
     }
+}
+int main(void)
+{
+
+	listint_t *head;
+	listint_t *current;
+	listint_t *reset;
+	clock_t start;
+	clock_t end;
+	clock_t diff;
+	int i;
+
+	head = NULL;
+	for (i = 0; i < 1001; i++)
+		add_nodeint(&head, i);
+
+	current = head;
+	while (current->next != NULL)
+		current = current->next;
+	reset = current;
+	current->next = head;
+
+	start = clock();
+
+	for (i = 0; i < 10; i++)
+		check_cycle(head);
+
+	end = clock();
+
+	diff = (double)(end - start) / 10;
+
+	if (diff > 40)
+		printf("Runtime too long\n");
+	else
+		printf("OK\n");
+
+	reset->next = NULL;
+
+	free_listint(head);
+
+	return (0);
 }
