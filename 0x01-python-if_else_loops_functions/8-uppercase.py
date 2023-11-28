6c5a981f1bb514dc06a7afd770d95a943ee415c6
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-28 19:57:52 +0200

    auto push

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if (ord(i) >= 97 and ord(i) < 123):
+            print("{}".format(chr(ord(i) + 55)), end='')
+        else:
+            print(i, end='')
+    print()
