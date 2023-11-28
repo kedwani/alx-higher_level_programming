0ffc9a71498a2bc692ebb83dfd9cebfa77443679
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-29 00:10:52 +0200

    auto push

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *h = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (h->n <= number && h->next != NULL)
+	{
+		if (h->next->n >= number)
+		{
+			new->next = h->next;
+			h->next = new;
+			return (new);
+		}
+	}
+	if (h->next == NULL)
+		h->next = new;
+	free(new);
+	return (NULL);
+}
--- 0x01-python-if_else_loops_functions/koko.py
+++
@@ -1,2 +0,0 @@
-str = ""
-print(len(str))
