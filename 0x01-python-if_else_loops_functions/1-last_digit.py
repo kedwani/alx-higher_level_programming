007edfa08fdc4817eb1955cdc783d7001c22e5c3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-11-28 17:53:27 +0200

    auto push

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if (number >= 0):
     last = number % 10
 else:
     last = -((-1 * number) % 10)
-if(last > 5):
+if (last > 5):
     print("Last digit of", number, "is", last, "and is greater than 5")
 elif (last == 0):
     print("Last digit of", number, "is", last, "and is 0")
