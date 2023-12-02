445098e32fd21de80c2e90087e43030df50c1c3b
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-03 00:16:06 +0200

    auto push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     from add_0 import add
     a = sys.argv[1]
     b = sys.argv[2]
+    import sys
     number = add(a, b)
     print("{} + {} = {}".format(a, b, number)
