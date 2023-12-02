358748118373ad9b2418b0cf79ca9c879b2cd1a3
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-03 00:09:41 +0200

    auto push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-number = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, number)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    number = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, number)
