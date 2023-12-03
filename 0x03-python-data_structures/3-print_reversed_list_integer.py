6ee6102fbb4a3ed29cc78ae0b7012ddd66f4e317
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-03 22:30:54 +0200

    auto push

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     n = -len(my_list)
     m = -1
     while (m >= n):
-        print("{}".format(my_list[m]))
+        print("{:d}".format(my_list[m]))
         m -= 1
