c0c7b9c73d367161a56aaf556ce9a4ce0c939491
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-04 00:18:51 +0200

    auto push

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    s = ""
+    i = 0
+    while (my_string[i]):
+        if (my_string[i] != 'c' and  my_string[i] != 'C'):
+            s += my_string[i]
+        i += 1
+    return (s)
