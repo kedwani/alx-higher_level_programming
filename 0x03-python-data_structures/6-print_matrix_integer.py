e621b0bfb1e3ea792f783963102e04c204d6818d
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-04 00:28:53 +0200

    auto push

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    while (i < len(matrix)):
+        j = 0
+        while (j < len(matrix[i])):
+            print("{} ".format(matrix[i][j]), end='')
+            j += 1
+        print()
+        i += 1
