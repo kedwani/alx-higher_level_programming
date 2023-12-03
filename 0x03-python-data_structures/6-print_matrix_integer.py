77f7f2049d9864c0ff3fb7dc76e53eef96245f0d
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-04 00:31:40 +0200

    auto push

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     while (i < len(matrix)):
         j = 0
         while (j < len(matrix[i])):
-            print("{:d} ".format(matrix[i][j]), end='')
+            if(j == len(matrix[i]) - 1):
+                print("{:d}".format(matrix[i][j]), end='')
+            else:
+                print("{:d} ".format(matrix[i][j]), end='')
+
             j += 1
         print()
         i += 1
