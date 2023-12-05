e61f007a315d3c59233743211da1a7858c3d7ba1
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-05 03:23:18 +0200

    auto push

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         result = (0, None)
-        return result
     else:
-        res = (length, sentence[0:1])
-        return res
+        result = (length, sentence[])
+    return res
