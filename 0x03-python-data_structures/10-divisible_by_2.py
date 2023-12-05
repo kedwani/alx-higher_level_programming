73d18686423efb9a2d8a93f1ecfe540457582778
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-05 03:48:24 +0200

    auto push

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_l = my_list.copy()
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0:
-            new_l[i] = 1
+    l = my_list.copy()
+    for i in range(0, len(l)):
+        if (my_list[i] % 2 == 0):
+            l[i] = True
         else:
-            new_l[i] = 0
-    return new_l
+            l[i] = False
+    return l
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,13 +1,19 @@
-def max_integer(my_list=[]):
-    if (len(my_list) == 0):
-        return None
-    i = my_list[0]
-    for ii in my_list:
-        if (ii > i):
-            i = ii
-    return i
+def divisible_by_2(my_list=[]):
+    s = my_list.copy()
+    for i in range(0,len(s)):
+        if ((my_list[i] % 2) == 0):
+            s[i] = True
+        else:
+            s[i] = False
+    print(len(s))
+    return (s)
 
 
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
