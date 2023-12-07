9f1524ec2384423a38359f61c27d0b1a929ab853
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 04:25:04 +0200

    auto push

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1.py import *
+    from calculator_1 import add,sub,div,mul
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
