c98582b18ff164c327ddc49b5a11709ebba2c818
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-07 04:20:41 +0200

    auto push

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-import sys
-from calculator_1.py import *
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-res = 0
-if (sys.argv[2] == '+'):
-    res = add(int(sys.argv[1]),int(sys.argv[3]))
-elif (sys.argv[2] == '-'):
-    res = sub(int(sys.argv[1]),int(sys.argv[3]))
-elif (sys.argv[2] == '*'):
-    res = mul(int(sys.argv[1]),int(sys.argv[3]))
-elif (sys.argv[2] == '/'):
-    res = div(int(sys.argv[1]),int(sys.argv[3]))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), res))
+if __name__ == __main__:
+    import sys
+    from calculator_1.py import *
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    res = 0
+    if (sys.argv[2] == '+'):
+        res = add(int(sys.argv[1]),int(sys.argv[3]))
+    elif (sys.argv[2] == '-'):
+        res = sub(int(sys.argv[1]),int(sys.argv[3]))
+    elif (sys.argv[2] == '*'):
+        res = mul(int(sys.argv[1]),int(sys.argv[3]))
+    elif (sys.argv[2] == '/'):
+        res = div(int(sys.argv[1]),int(sys.argv[3]))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), res))
