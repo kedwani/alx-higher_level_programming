01584682633a820f5d4078dc76c26f4e2800e823
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:05:10 +0200

    auto push

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
+"""
+a file contain write_file function
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    rite text to a file.
+
+    This function writes the provided text to a file specified by the filename.
+
+    Parameters:
+    filename (str): name of the file to write to. Defaults to an empty string.
+    text (str): text to be written to the file. Defaults to an empty string.
+
+    Returns:
+    int: The number of characters written to the file.
+
+
+    """
     with open(filename, "w", emcoding="utf-8") as f:
         return (f.write(text))
