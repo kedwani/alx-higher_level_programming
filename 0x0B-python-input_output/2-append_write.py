c580a77fb84867a407299a2d7ce8ed218a31a2bc
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:12:53 +0200

    auto push

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+file contain append_write function
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    a function to append text to a file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
