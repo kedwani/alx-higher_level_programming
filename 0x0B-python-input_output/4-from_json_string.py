585d2808c40a9aeae5120effed15f70f99ad4e18
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:39:03 +0200

    auto push

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+contain a function
+"""
+
+
+def from_json_string(my_str):
+    """
+    a functio to reload json
+    """
+    import json
+    return (json.loads(my_str))
