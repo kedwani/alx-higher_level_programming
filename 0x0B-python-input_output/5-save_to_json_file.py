200c3b52b4e484ea2a609ad62f534511a4f3e14f
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:45:22 +0200

    auto push

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+conatain a function
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    a function to make an action
+    """
+    import json
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
