5cf95ca54d81b9781ecc798b5a06b62a91c6af98
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:30:17 +0200

    auto push

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+file contain function
+"""
+
+
+def to_json_string(my_obj):
+    """
+    a function to represent the json
+    """
+    import json
+    return (json.dumps(my_obj))
