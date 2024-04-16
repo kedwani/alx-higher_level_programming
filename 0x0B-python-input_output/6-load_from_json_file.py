cec60539f099f5040f6334223f43fd5e8ad45d09
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 01:48:20 +0200

    auto push

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+contain a function
+"""
+
+
+def load_from_json_file(filename):
+    """
+    this is a function
+    """
+    import json
+    with open(filename, encoding="utf-8") as f:
+        return (json.load(f))
