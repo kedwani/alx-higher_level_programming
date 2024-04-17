242b647775ce9a6f34eabd186d1167c6283396de
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 02:47:40 +0200

    auto push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,10 @@
 a module to do something
 """
 
-
 import sys
 import json
-__import__("5-save_to_json_file").save_to_json_file
-__import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 a = sys.argv[1:]
-with open("add_item.json", "w", encoding="utf-8") as f:
-    save_to_json_file(a,f)
+save_to_json_file(a, "add_item.json")
+load_from_json_file ("add_item.json")
