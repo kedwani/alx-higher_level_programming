5af10caf7f1f506fb8f0cff60d051f8d1365d18a
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 02:40:48 +0200

    auto push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,4 +10,4 @@ __import__("5-save_to_json_file").save_to_json_file
 __import__("6-load_from_json_file").load_from_json_file
 a = sys.argv[1:]
 with open("add_item.json", "w", encoding="utf-8") as f:
-    json.dump(a, f)
+    save_to_json_file(a,f)
