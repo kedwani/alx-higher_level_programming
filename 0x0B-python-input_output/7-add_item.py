57340a92dee48dbf6c7acc908b76f6e8048fd4af
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-17 03:10:37 +0200

    auto push

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,19 @@ a module to do something
 """
 
 import sys
+import os
 import json
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-a = sys.argv[1:]
-save_to_json_file(a, "add_item.json")
+l = sys.argv[1:]
+a = []
+for i in l:
+    a.append(i)
+if os.path.exists("add_item.json"):
+    c = load_from_json_file ("add_item.json")
+    c += a
+    save_to_json_file(c, "add_item.json")
+
+else:
+    save_to_json_file(a, "add_item.json")
 load_from_json_file ("add_item.json")
